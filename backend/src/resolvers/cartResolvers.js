043c6fddea74dde72fa3ba809c3d591a0b4f3a27
CarlaOnate <carlaonategardella@gmail.com>
2021-10-30 16:39:52 -0500

    Finished calculating cart price

--- backend/src/resolvers/cartResolvers.js
+++ backend/src/resolvers/cartResolvers.js
@@ -33,9 +33,11 @@ const cartResolvers = {
             } else {
                 cart.items.push({item: product.id, qty: 1})
             }
+
             await cart.save()
+            await cartResolvers.Mutation.calculatePrice(cartId)
+
             await cart.populate({path: 'items', populate: [{path: 'item', ref: 'Product'}]})
-            await cartResolvers.Mutation.calculateDiscount(_,cartId)
 
             return "Added item"
         },
@@ -55,37 +57,46 @@ const cartResolvers = {
                 }
             }
             await cart.save()
+            await cartResolvers.Mutation.calculatePrice(cartId)
 
             return "Deleted item"
         },
-        calculatePrice: async (_, {cartId}) => {
+        calculatePrice: async (cartId) => {
             console.log("calculateDis inside", cartId)
             const cart = await Cart.findById(cartId).populate({path: 'items', populate: [{path: 'item', ref: 'Product'}]})
-            // cart.discount = 0
+            cart.discount = 0
             console.log(cart)
-
+            //Calculate discounts
             if(cart.items.length > 0){
-                const ndaIndex = cart.items.find(el => el.item.type === "Nda")
-                const termSheetIndex = cart.items.find(el => el.item.type === "TermSheet")
+                const ndaIndex = cart.items.findIndex(el => el.item.type === "Nda")
+                const termSheetIndex = cart.items.findIndex(el => el.item.type === "TermSheet")
+                console.log("Indexes", ndaIndex, termSheetIndex)
 
-                if(ndaIndex){
+                if(ndaIndex !== -1){
                 //2 of Nda items menas price of one
+                    console.log(cart.items, cart.items[ndaIndex])
                     const {qty} = cart.items[ndaIndex]
                     const {price} = cart.items[ndaIndex].item
-                    //
                     qty % 2 === 0 ? cart.discount += (price*qty)/2 : cart.discount += ((qty-1)*price)/2
                     console.log("Nda", cart.discount, qty)
                 }
-                if (termSheetIndex){
+                if (termSheetIndex !== -1){
                 //3 or more Termsheet then price is 100 each
-                    const {qty} = cart.items[ndaIndex]
-                    const {price} = cart.items[ndaIndex].item
+                    console.log(cart.items, cart.items[termSheetIndex])
+                    const {qty} = cart.items[termSheetIndex]
+                    const {price} = cart.items[termSheetIndex].item
                     if(qty > 2) cart.discount += (price-100)*qty
                     console.log("TermSheet", cart.discount, qty)
                 }
+
+                cart.subtotal = cart.items.reduce((prev, current) => (prev.qty*prev.item.price)+(current.qty*current.item.price))
+                cart.total = cart.subtotal*1.16
+
+
+                cart.save()
+                console.log(cart)
+
             }
-
-
 
             return cart
 
