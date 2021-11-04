ce9dcc829a5f86aed5957f354e02befae82465c0
CarlaOnate <carlaonategardella@gmail.com>
2021-11-04 14:50:28 -0600

    Starting styles

--- front/src/App.js
+++ front/src/App.js
@@ -3,70 +3,9 @@ import Cart from "./components/Cart"
 import { useMutation, gql } from "@apollo/client";
 import styled from 'styled-components'
 import {useState} from "react";
+import "./styles/App.css"
 
-//Estos son los estilos de este componente
-const AppDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    gap: 10px;
-    h1 {
-       margin: 5% 10px 0px 10px;
-       font-size: 15px; 
-       color: #302f2f;
-    }
-    p {
-        margin: 2px 10px 20px 10px;
-    }
-    &>div{
-        display: flex;
-        flex-direction: column;
-        flex-grow: 2;
-    }
-    @media screen 
-    and (min-device-width : 375px) 
-    and (max-device-width : 768px){
-      font-size: 15px;
-    }
-    @media screen 
-    and (min-device-width : 768px) 
-    and (max-device-width : 1024px){
-        h1 {
-            margin-top: 5vh; 
-            font-size: 25px;            
-        }
-        &>div {
-            gap: 15px;
-            margin: 2%;
-            flex-direction: row;
-            align-items: flex-start;  
-        } 
-        p {
-            font-size: .9rem;
-        }
-    }
-    @media screen 
-    and (min-device-width : 1024px) 
-    and (max-device-width : 1281px){
-        align-items: center;
-        h1 {
-            width: 80%;
-            margin-top: 5vh; 
-            font-size: 25px;            
-        }
-        &>div {
-            gap: 5%;
-            margin: 5%;
-            width: 80%;
-            flex-direction: row;
-            align-items: flex-start;  
-        } 
-        &>p {
-            width: 80%;
-            font-size: .9rem;
-        }
-    }
-`
+
 
 //Se define la mutación para guardar el carrito en la DB
 //Esto se llama aquí porque se hace conditional rendering dependiendo del resultado de esta mutation
@@ -113,14 +52,14 @@ function App() {
 
     //Se llaman los dos componentes de la app y se les pasan los datos por props
     return (
-    <AppDiv>
+    <div className="app">
         <h1>Selecciona los contratos que necesitas:</h1>
         <p>Eliges todos los documentos que necesites y realiza tu pago. Contéstalos y descárgalos cuando los necesites.</p>
         <div>
              <Menu setChange={setChange} cartState={{cart, setCart}}/>
              <Cart change={change} cartState={{cart, setCart}} saveCart={{saveCart, saveCartRes}}/>
         </div>
-    </AppDiv>
+    </div>
   );
 }
 
